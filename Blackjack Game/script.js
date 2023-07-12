ed465915f66c2161eda4df158fc2e54b4d3f94a5
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-07-12 13:33:11 +0530

    Add files via upload

--- Blackjack Game/script.js
+++ Blackjack Game/script.js
@@ -4,19 +4,35 @@ let sum = firstCard+secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messEl = document.querySelector('#mess-el')
+let cardEl = document.querySelector('#cards-el')
+let sumEl = document.querySelector('#sum-el')
 
-if(sum <= 20){
-    console.log("Want to draw a Card ??")
-    message("Withdrawl a Card")
+function newCard(){
+    console.log("Draw a new Card : ")
+    let nCard = 7
+    sum += nCard 
 }
-else if(sum === 21){
-    console.log("WOOH!! Its a BLACK JACK")
-    hasBlackJack = true
+
+function startGame(){
+    cardEl.textContent = "Cards : " +firstCard +"  " +secondCard
+    sumEl.textContent = "Sum : " +sum
+
+    if(sum <= 20){
+        console.log("Want to draw a Card ??")
+        message("Withdrawl a Card")
+    }
+    else if(sum === 21){
+        console.log("WOOH!! Its a BLACK JACK")
+        hasBlackJack = true
+    }
+    else{
+        console.log("You lost the Game")
+        message("Game Over")
+        isAlive = false
+    }
+    messEl.textContent = message
+    console.log(message)
+    console.log(hasBlackJack)
+    console.log(isAlive)
 }
-else{
-    console.log("You lost the Game")
-    message("Game Over")
-    isAlive = false
-}
-console.log(hasBlackJack)
-console.log(isAlive)
